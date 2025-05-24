a38103656df21e662b658090b1f4a54c726f1e72
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2025-05-24 20:17:23 +0530

    created dtos with input validation attributes

---
+++ LoanLens.Domain/DTOs/LoanResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LoanLens.Domain.DTOs
+{
+    public class LoanResultDto
+    {
+        public decimal EMI { get; set; }
+        public decimal EMIRatio { get; set; }
+        public string? RiskCategory { get; set; }
+        public bool IsEligible { get; set; }
+    }
+}
